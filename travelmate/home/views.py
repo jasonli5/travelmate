f1a056b6696a02156e90891ede9d8c6e48581f6d
Ari Hammond <ajhammond06@gmail.com>
2025-04-08 13:54:28 -0400

    home page and header/footer for base template

---
+++ travelmate/home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    template_data = {}
+    template_data['title'] = 'TravelMate'
+    return render(request, 'home/index.html', {'template_data': template_data})
+
