96896666b390f32b8c5de44c90e33745143b2df6
MithilCherkadi <144705595+MithilCherkadi@users.noreply.github.com>
2025-04-14 20:32:15 -0400

    Revert "added functionality to save, edit, and delete trips"

--- travelmate/trips/forms.py
+++ travelmate/trips/forms.py
@@ -1,14 +1,7 @@
-from django import forms
 from django.forms import ModelForm
-from .models import Trip, inputTrip
-
-class TripForm(forms.ModelForm):
-    class Meta:
-        model = Trip
-        fields = ['destination', 'start_date', 'end_date', 'notes']
+from .models import inputTrip
 
 class InputForm(ModelForm):
     class Meta:
         model = inputTrip
         fields = ['destination', 'start_date','end_date','activities']
-
