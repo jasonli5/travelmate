077783ff4d0abc9623e2c125b19ef9b1b566e27e
jasonli5 <jasonli2068@gmail.com>
2025-04-18 21:10:06 -0400

    remove unneeded fields & migrate

--- travelmate/ai/admin.py
+++ travelmate/ai/admin.py
@@ -4,7 +4,6 @@ from .models import Activity
 
 @admin.register(Activity)
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'trip', 'is_ai_suggested')
-    search_fields = ('name', 'description')
-    list_filter = ('is_ai_suggested',)
+    list_display = ('name', 'trip')
+    search_fields = ('name',)
     list_per_page = 25
--- travelmate/ai/models.py
+++ travelmate/ai/models.py
@@ -6,9 +6,7 @@ class Activity(models.Model):
     Model representing an activity.
     """
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
     trip = models.ForeignKey(inputTrip, on_delete=models.CASCADE, related_name='activities')
-    is_ai_suggested = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
