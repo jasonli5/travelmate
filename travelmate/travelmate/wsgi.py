fbd8f766a7e2c23550487f76e41fc8baf4e1ec33
jasonli5 <jasonli2068@gmail.com>
2025-04-27 13:55:59 -0400

    add support for serving uploaded images

--- travelmate/travelmate/wsgi.py
+++ travelmate/travelmate/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travelmate.settings")
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+application.add_files(os.path.join(os.path.dirname(__file__), 'media'), prefix='media/')
